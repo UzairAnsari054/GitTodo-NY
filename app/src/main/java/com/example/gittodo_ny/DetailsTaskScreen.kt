784217957d6bdd63d104a1cafa6ac8a439d41bcd
Uzair Ansari <124576222+UzairAnsari054@users.noreply.github.com>
2024-11-12 15:16:09 +0530

    details-task-screen

--- app/src/main/java/com/example/gittodo_ny/DetailsTaskScreen.kt
+++ app/src/main/java/com/example/gittodo_ny/DetailsTaskScreen.kt
@@ -1,2 +1,9 @@
 package com.example.gittodo_ny
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun details (modifier: Modifier = Modifier) {
+    
+}
